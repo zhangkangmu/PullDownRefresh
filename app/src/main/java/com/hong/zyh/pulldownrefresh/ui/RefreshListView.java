e9884e33e6c2939837f2c7199d4906bf909839d9
zhangyuhong <289590351@qq.com>
2019-02-28 16:30:40 +0800

    增加头部刷新隐藏功能，方法：测量规则，测量头部具体隐藏

--- app/src/main/java/com/hong/zyh/pulldownrefresh/ui/RefreshListView.java
+++ app/src/main/java/com/hong/zyh/pulldownrefresh/ui/RefreshListView.java
@@ -3,6 +3,7 @@ package com.hong.zyh.pulldownrefresh.ui;
 import android.content.Context;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 
@@ -41,7 +42,21 @@ public class RefreshListView extends ListView {
     }
 
     private void initHeaderView() {
+        //layout_header_list整个头布局的变量
         layout_header_list = View.inflate(getContext(), R.layout.layout_header_list, null);
+
+        //真实的头布局高度，就算加上padding后也不会改变
+        layout_header_list.getHeight();
+
+        //根据测量规则测量，（不要忘了这个，没有这个则每次都是0）
+        layout_header_list.measure(0,0);
+
+        //测量高度，显示在页面中的高度
+        int measuredHeight = layout_header_list.getMeasuredHeight();
+        Log.d("initHeaderView",measuredHeight+"");
+
+        //设置头布局的内边距，让他默认移出页面，也就是在页面最上方，但是移出了手机页面
+        layout_header_list.setPadding(0,-measuredHeight,0,0);
         addHeaderView(layout_header_list);
     }
 }
