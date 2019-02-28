657482f75f1192d010a12d9498523276a4dccec4
zhangyuhong <289590351@qq.com>
2019-02-28 17:51:25 +0800

    增加头布局下拉显示的功能，方法：通过设置触摸事件判断偏移量来决定头布局的measuredHeight

--- app/src/main/java/com/hong/zyh/pulldownrefresh/ui/RefreshListView.java
+++ app/src/main/java/com/hong/zyh/pulldownrefresh/ui/RefreshListView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ListView;
 
@@ -17,6 +18,18 @@ import com.hong.zyh.pulldownrefresh.R;
 public class RefreshListView extends ListView {
 
     private View layout_header_list;
+    private float downY;
+    private float upY;
+    private float moveY;
+    private int measuredHeight;
+    //头部完全显示的状态，下拉刷新
+    public static final int PULL_TO_REFRESH =0;
+    //刷新完毕状态
+    public static final int RELEASE_REFRESH=1;
+    //正在刷新的状态，这时候头部完全显示的状态
+    public static final int REFRESHING=2;
+    //当前的刷新模式
+    private int currentState= PULL_TO_REFRESH;
 
     public RefreshListView(Context context) {
         super(context);
@@ -52,11 +65,56 @@ public class RefreshListView extends ListView {
         layout_header_list.measure(0,0);
 
         //测量高度，显示在页面中的高度
-        int measuredHeight = layout_header_list.getMeasuredHeight();
-        Log.d("initHeaderView",measuredHeight+"");
+        measuredHeight = layout_header_list.getMeasuredHeight();
+        Log.d("initHeaderView", measuredHeight +"");
 
         //设置头布局的内边距，让他默认移出页面，也就是在页面最上方，但是移出了手机页面
         layout_header_list.setPadding(0,-measuredHeight,0,0);
         addHeaderView(layout_header_list);
+    }
+
+
+    /**
+     * 下落显示，通过判断偏移量实现
+     *
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                downY = ev.getY();
+                Log.d("zhangyuhong-downY",""+ downY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                //往下拖动的时候moveY会慢慢变大
+                moveY = ev.getY();
+                Log.d("zhangyuhong-moveY", "" + moveY);
+                //向下划动的时候的偏移量
+                float offset = moveY - downY;
+                //向下滑动=-自身高对+偏移量
+                int paddingTop = (int) (-measuredHeight + offset);
+                // 只有 偏移量>0, 并且当前第一个可见条目索引是0, 才放大头部
+                if (offset > 0 && getFirstVisiblePosition() == 0) {
+                    layout_header_list.setPadding(0, paddingTop, 0, 0);
+
+                    if (paddingTop>=0 && currentState!=RELEASE_REFRESH){
+                        currentState=RELEASE_REFRESH;
+                        Log.d("偏移量paddingTop>=0---释放模式",""+currentState);
+                    }else if (paddingTop<0 && currentState!= PULL_TO_REFRESH){
+                        currentState=PULL_TO_REFRESH;
+                        Log.d("偏移量paddingTop<0下落刷新模式模式",""+currentState);
+                    }
+                    //true表示当前事件被消费
+                    return true;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                upY = ev.getY();
+                Log.d("zhangyuhong-upY", "" + upY);
+                break;
+            default:
+                break;
+        }
+        return super.onTouchEvent(ev);
     }
 }
